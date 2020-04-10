02b6dbf178804b6ea3278bc275840ca4c66f0c3a
kjpayne <62717712+kjpayne@users.noreply.github.com>
2020-04-10 10:42:23 -0600

    update Exercise_2_13

--- Chapter 2/Exercise_2_13.java
+++ Chapter 2/Exercise_2_13.java
@@ -15,26 +15,26 @@ public class Exercise_2_13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-    // Entering monthly saving
-    System.out.print("Enter monthly saving amount: ");
-    double monthlysaving = input.nextDouble();
+        // Entering monthly saving
+        System.out.print("Enter monthly saving amount: ");
+        double monthlysaving = input.nextDouble();
     
-    // Monthly interest
-    final double MONTHLYINTEREST = (1 + 0.00417);
+        // Monthly interest
+        final double MONTHLYINTEREST = (1 + 0.00417);
     
-    // Account value after one month
-    double accountvalue = monthlysaving * MONTHLYINTEREST;
+        // Account value after one month
+        double accountvalue = monthlysaving * MONTHLYINTEREST;
     
-    // Account value after two months
-    accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
+        // Account value after two months
+        accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
     
-    // Account value after three to six months
-    accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
-    accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
-    accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
-    accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
+        // Account value after three to six months
+        accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
+        accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
+        accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
+        accountvalue = (monthlysaving + accountvalue) * MONTHLYINTEREST;
     
-    // Display account value after six months
-    System.out.println("After the sixth month, the account value is " + accountvalue);
+        // Display account value after six months
+        System.out.println("After the sixth month, the account value is " + accountvalue);
     }
 }
